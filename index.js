5a0cc4b78ebcf677d7af360b8c57f161d5a95869
oshhh544 <81898027+oshhh544@users.noreply.github.com>
2023-03-28 22:47:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => {
-  console.log('Server has started on port 3000... ')
+  console.log('Server has started on port 3000')
 })
 
+app.get('/meeetUps', function(req, res){
+  res.send(meetUps)
+})
 
-class MeetUp{
+app.get('/meeetUps/:id', function(req, res){
+  meetups.forEach(element => {
+    if(element.id == req.params.id)
+    res.send(element)
+  });
+})
+
+app.delete('/meetups/:id', function(req, res){
+  meetups.forEach((element, index) => {
+    if(element.id == req.params.id)
+    {
+      meetUps.slice(index, 1)
+      res.status(201).end()
+    }
+  }
+)}
+  )
+  
+
+class Meetup{
   constructor(id, title, description, keywords, time, place) {
     this.id  = id
     this.title = title
@@ -25,5 +47,7 @@ class MeetUp{
   }
   
 }
+ let meetups = [new Meetup(2, 'конференция', ' ', [' ', ' ', ' ' ], '16.06.2026', ' '), new Meetup(3, 'конференция', ' ', [' ', ' ', ' ' ], '16.06.2026', ' ')]
+ 
 
 
