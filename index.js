e0958c885d6ae0994dbcb97803cb30226ac755e8
oshhh544 <81898027+oshhh544@users.noreply.github.com>
2023-04-03 21:37:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
 const express = require('express')
 const app = express()
+const meetupsRouter = require('./routes/meetup.routes')
+
+app.use(express.json())
+app.use('./api', meetupRouter)
 
 app.listen(3000, () => {
   console.log('Server has started on port 3000')
 })
-
-app.get('/meeetups', function(req, res){
-  res.send(meetups)
-})
-
-app.get('/meeetups/:id', function(req, res){
-  meetups.forEach(element => {
-    if(element.id == req.params.id)
-    res.send(element)
-  });
-})
-
-app.delete('/meetups/:id', function(req, res){
-  meetups.forEach((element, index) => {
-    if(element.id == req.params.id)
-    {
-      meetups.slice(index, 1)
-      res.status(201).end()
-    }
-  }
-)}
-  )
-  
-
-class Meetup{
-  constructor(id, title, description, keywords, time, place) {
-    this.id  = id
-    this.title = title
-    this.description = description
-    this.keywords = keywords
-    this.time = time
-    this.place = place
-  }
-
-  showAll(){
-    console.log(`Тема: ${this.title}, Описание: ${this.description}, Ключевые слова: ${this.keywords} , Время: ${this.time}, Место: ${this.place}`)
-  }
-
-  create(id, title, description, keywords, time, place) {
-    return new Meetup(id, title, description, keywords, time, place)
-  }
-  
-}
- let meetups = [new Meetup(2, 'конференция', ' ', [' ', ' ', ' ' ], '16.06.2026', ' '), new Meetup(3, 'конференция', ' ', [' ', ' ', ' ' ], '16.06.2026', ' ')]
- 
-
 
