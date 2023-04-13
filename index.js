b58c47a9b64a0bf58d92566d5e86179f8b819c8c
oshhh544 <81898027+oshhh544@users.noreply.github.com>
2023-04-13 21:29:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const meetupsRouter = require('./routes/meetup.routes')
+const meetupRouter = require('./routes/meetup.routes')
 
 app.use(express.json())
 app.use('/api', meetupRouter)
@@ -8,4 +8,3 @@ app.use('/api', meetupRouter)
 app.listen(3000, () => {
   console.log('Server has started on port 3000')
 })
-
