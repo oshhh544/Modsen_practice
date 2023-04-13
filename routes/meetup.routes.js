8d755318da4889ab53a8665561fa292608bd06b8
oshhh544 <81898027+oshhh544@users.noreply.github.com>
2023-04-13 19:44:44 +0300

    Update meetup.routes.js

--- routes/meetup.routes.js
+++ routes/meetup.routes.js
@@ -3,7 +3,7 @@ const router = new Router()
 const meetupController = require('../controller/meetup.controller')
 
 router.post('/meetups',meetupController.create)
-router.get('/meetups/:id',meetupController.show)
+router.get('/meetups/:id',meetupController.get)
 router.get('/meetups/:id',meetupController.getById)
 router.put('/meetups',meetupController.update)
 router.delete('/meetups/:id',meetupController.delete)
